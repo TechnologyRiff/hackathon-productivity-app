7c373a658ead3434940d3eb828fc0aec8869a6f0
Morgan Riffer <technologyriff@gmail.com>
2016-02-20 15:56:05 -0500

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,19 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @users = User.all
   end
 
   def show
   end
 
+  def edit
+  end
+
   def update
   end
 
-  def destroy
-  end
+private
 
   def user_params
     params.require(:user).permit(:name, :role, :avatar)
