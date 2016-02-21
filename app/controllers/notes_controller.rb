5dbe6c399f2588df36de8ff50a36921e0e5ea4e8
Jason Data <jason.data@roadbytes.me>
2016-02-21 09:17:23 -0800

    added simple features for MVP

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,11 +6,20 @@ class NotesController < ApplicationController
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def update
+    note = Note.find(params[:id])
+    note.update(note_params)
+    redirect_to user_path(current_user)
   end
 
   def index
+  end
+  
+  private
+  def note_params
+    params.require(:note).permit(:summary)
   end
 end
